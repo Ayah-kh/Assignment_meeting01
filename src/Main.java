e0da4b31edade54cc9718d5b09aa5486189f2b43
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-06-04 12:49:39 +0300

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+}
